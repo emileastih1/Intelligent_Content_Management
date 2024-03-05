65ed25a1d245b1986a9b086ece6c071fe02f7186
Emile ASTIH <emileastih1@gmail.com>
2024-03-05 15:53:26 +0100

    -docker:  * Changed to the custom pgvector image to support vectorstore extension  * Updated liquibase files to created the needed objects

--- src/main/resources/db/changelog/DocumentContent-DOC_MANAGEMENT_DB/sprint1/release1/ddl/CREATE-VECTOR-SCHEMA.sql
+++ src/main/resources/db/changelog/DocumentContent-DOC_MANAGEMENT_DB/sprint1/release1/ddl/CREATE-VECTOR-SCHEMA.sql
@@ -1,1 +1,12 @@
 CREATE SCHEMA IF NOT EXISTS vectorcontent;
+
+SET SEARCH_PATH TO vectorcontent;
+
+CREATE EXTENSION IF NOT EXISTS vector;
+
+CREATE TABLE IF NOT EXISTS embeddings (
+  id SERIAL PRIMARY KEY,
+  embedding vector,
+  text text,
+  created_at timestamptz DEFAULT now()
+);
