c1154dd03bc08792f7b296e98d2918c5b98170bf
Emile ASTIH <emileastih1@gmail.com>
2024-03-07 21:03:42 +0100

    - Implemented the api part for ai chat with RAG - Now you can upload your document and then ask openai questions related to those documents.

--- src/main/java/com/ea/architecture/domain/driven/infrastructure/repository/document/DocumentAiRepository.java
+++ src/main/java/com/ea/architecture/domain/driven/infrastructure/repository/document/DocumentAiRepository.java
@@ -1,22 +1,35 @@
 package com.ea.architecture.domain.driven.infrastructure.repository.document;
 
 import com.ea.architecture.domain.driven.domain.document.model.DocumentAggregate;
+import com.ea.architecture.domain.driven.domain.document.vo.ai.Answer;
+import com.ea.architecture.domain.driven.domain.document.vo.ai.Question;
+import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.reader.tika.TikaDocumentReader;
 import org.springframework.ai.transformer.splitter.TextSplitter;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class DocumentAiRepository {
-    VectorStore vectorStore;
+    private final VectorStore vectorStore;
+    private final PromptTemplate promptTemplate;
+    private final ChatClient aiChatClient;
 
-    public DocumentAiRepository(VectorStore vectorStore) {
+    public DocumentAiRepository(VectorStore vectorStore, PromptTemplate promptTemplate, ChatClient aiChatClient) {
         this.vectorStore = vectorStore;
+        this.promptTemplate = promptTemplate;
+        this.aiChatClient = aiChatClient;
     }
 
     public void addDocumentToVectorStore(DocumentAggregate documentAggregate) {
@@ -24,13 +37,23 @@ public class DocumentAiRepository {
         TikaDocumentReader tikaDocumentReader = new TikaDocumentReader(byteArrayResource);
         List<Document> documents = tikaDocumentReader.get();
 
-/*        Document document = new Document("Spring AI rocks!! Spring AI rocks!!" +
-                " Spring AI rocks!! Spring AI rocks!! Spring AI rocks!!", Map.of("meta1", "meta1"));
-
-        List<Document> documents = List.of(document);*/
-
         TextSplitter textSplitter = new TokenTextSplitter();
         List<Document> splitDocuments = textSplitter.apply(documents);
         vectorStore.add(splitDocuments);
+    }
+
+    public Answer askRelevantQuestion(Question question) {
+        List<Document> similarDocuments = vectorStore.similaritySearch(SearchRequest.query(question.question()).withTopK(2));
+        if (similarDocuments != null && !similarDocuments.isEmpty()) {
+            List<String> contentList = similarDocuments.stream().map(Document::getContent).toList();
+            Map<String, Object> promptParameters = new HashMap<>();
+            promptParameters.put("input", question.question());
+            promptParameters.put("documents", String.join("\n", contentList));
+            Prompt prompt = promptTemplate.create(promptParameters);
+            ChatResponse response = aiChatClient.call(prompt);
+            return new Answer(response.getResult().getOutput().getContent());
+        } else {
+            return new Answer("Sorry, I don't have an answer for that question");
+        }
     }
 }
--- src/main/java/com/ea/architecture/domain/driven/presentation/config/ai/VectorStoreEmbeddingClient.java
+++ src/main/java/com/ea/architecture/domain/driven/presentation/config/ai/VectorStoreEmbeddingClient.java
@@ -1,16 +1,23 @@
 package com.ea.architecture.domain.driven.presentation.config.ai;
 
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.embedding.EmbeddingClient;
 import org.springframework.ai.vectorstore.PgVectorStore;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import static org.springframework.ai.vectorstore.PgVectorStore.OPENAI_EMBEDDING_DIMENSION_SIZE;
 
 @Configuration
 public class VectorStoreEmbeddingClient {
+
+    @Value("classpath:/ai/promptTemplate/rag-document-prompt-template.st")
+    private Resource ragPromptTemplate;
+
     /**
      * description: It is very important to pass the desired dimension to the pgVectorStore in our case OPENAI_EMBEDDING_DIMENSION_SIZE
      * Because if you don't pass in the dimension, a call will be made to the openAI API to get the dimension size.
@@ -24,5 +31,10 @@ public class VectorStoreEmbeddingClient {
     @Bean
     public VectorStore vectorStore(JdbcTemplate jdbcTemplate, EmbeddingClient embeddingClient) {
         return new PgVectorStore(jdbcTemplate, embeddingClient, OPENAI_EMBEDDING_DIMENSION_SIZE);
+    }
+
+    @Bean
+    public PromptTemplate promptTemplate() {
+        return new PromptTemplate(ragPromptTemplate);
     }
 }
